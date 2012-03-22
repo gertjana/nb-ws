9acc3373004c38999f7e6515a57c644cddc0cc83
Gertjan Assies <gertjan.assies@gmail.com>
2012-03-22 14:21:20 +0100

    Unittest for parsing routes

--- src/test/java/net/addictivesoftware/nbws/TestRoutes.java
+++ src/test/java/net/addictivesoftware/nbws/TestRoutes.java
@@ -1,11 +1,38 @@
 package net.addictivesoftware.nbws;
 
-/**
- * Created by IntelliJ IDEA.
- * User: gertjan
- * Date: 3/22/12
- * Time: 9:04 AM
- * To change this template use File | Settings | File Templates.
- */
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TestRoutes {
+
+    @Test
+    public void testInitializeRoutes() throws IOException {
+        Routes r = new Routes("/routes");
+
+        Path path = r.getPathForUri("GET", "/user/4");
+        Assert.assertNotNull(path);
+        Assert.assertEquals("UserApi.getUser", path.getExecutableMethod());
+
+        path = r.getPathForUri("GET", "/static/test");
+        Assert.assertNotNull(path);
+        Assert.assertEquals("Default.handle", path.getExecutableMethod());
+
+        path = r.getPathForUri("PUT", "/some/path/");
+        Assert.assertNotNull(path);
+        Assert.assertEquals("Default.refuse", path.getExecutableMethod());
+
+    }
+
+    @Test
+    public void testPattern() {
+        String pattern = "^/user/([a-zA-Z0-9]+)$";
+        String uri = "/user/4";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(uri);
+        Assert.assertTrue(m.matches());
+    }
 }
