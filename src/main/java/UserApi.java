d10b2d42fdef2001936e8da6852e82f2f90fa8f9
Gertjan Assies <gertjan.assies@gmail.com>
2012-03-23 11:34:53 +0100

    calling routes/path from ServerHandler, simple implementations of all defined paths

--- src/main/java/UserApi.java
+++ src/main/java/UserApi.java
@@ -9,5 +9,21 @@ public class UserApi {
     public static String registerUser(HttpRequest request, String email, String name)  {
         return "registering " + name + " with email " + email;
     }
+    
+    public static String listUsers(HttpRequest request) {
+        return "listing all users ...";
+    }
+
+    public static String newUser(HttpRequest request) {
+        return "creating new user";
+    }
+    
+    public static String updateUser(HttpRequest request, String id) {
+        return "updating user with id " + id;
+    }
+    
+    public static String deleteUser(HttpRequest request, String id) {
+        return "deletin user with id " + id;
+    }
 
 }
